7cae5cd7889f349a15cc2435e9b51bb46a3acde2
Савелий <saveliikulakov@mail.ru>
2025-04-08 18:51:11 +0500

    Initial commit

---
+++ ArxProject3/acrxEntryPoint.cpp
@@ -0,0 +1,239 @@
+// (C) Copyright 2002-2012 by Autodesk, Inc. 
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted, 
+// provided that the above copyright notice appears in all copies and 
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting 
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS. 
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC. 
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+//
+// Use, duplication, or disclosure by the U.S. Government is subject to 
+// restrictions set forth in FAR 52.227-19 (Commercial Computer
+// Software - Restricted Rights) and DFAR 252.227-7013(c)(1)(ii)
+// (Rights in Technical Data and Computer Software), as applicable.
+//
+
+//-----------------------------------------------------------------------------
+//----- acrxEntryPoint.cpp
+//-----------------------------------------------------------------------------
+#include "StdAfx.h"
+#include "resource.h"
+
+#include "rxobject.h"
+#include <thread>
+#include <sstream>
+#include <iostream>
+#include "adui.h"
+#include "acdb.h"
+#include <mutex>
+#include <chrono>
+#include <aced.h>
+#include "acedCmdNF.h"
+
+//-----------------------------------------------------------------------------
+#define szRDS _RXST("ADSK")
+
+class CArxProject3App;
+
+
+
+CArxProject3App* g_pApp = nullptr;
+
+
+class CArxProject3App : public AcRxArxApp {
+
+public:
+	CArxProject3App() {}
+
+	virtual AcRx::AppRetCode On_kInitAppMsg (void *pkt) {
+		// TODO: Load dependencies here
+
+
+		InitApp();
+
+		//if (collission) {
+			//showMyWindow();
+		//}
+
+		m_lastRunTime = std::chrono::steady_clock::now();
+		
+		std::thread timerThread(&CArxProject3App::TimerFunction, this);
+		timerThread.detach();
+
+		// You *must* call On_kInitAppMsg here
+		AcRx::AppRetCode retCode =AcRxArxApp::On_kInitAppMsg (pkt) ;
+		
+		// TODO: Add your initialization code here
+
+		return (retCode) ;
+	}
+
+	virtual AcRx::AppRetCode On_kUnloadAppMsg (void *pkt) {
+		// TODO: Add your code here
+
+		// You *must* call On_kUnloadAppMsg here
+		AcRx::AppRetCode retCode =AcRxArxApp::On_kUnloadAppMsg (pkt) ;
+
+		// TODO: Unload dependencies here
+
+		return (retCode) ;
+	}
+
+	virtual void RegisterServerComponents() {
+	}
+
+	void CArxProject3App::InitApp() {
+		g_pApp = this;
+
+	}
+
+	void TimerFunction() {
+		while (true) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+			auto now = std::chrono::steady_clock::now();
+			
+			acedCommandS(RTSTR, L"._STARTTEST");			
+						
+			if (std::chrono::duration_cast<std::chrono::minutes>(now - m_lastRunTime).count() >= 10) {
+				
+				GetAllLines();
+				searchForOverlays();
+				m_lastRunTime = now;
+			}
+		}
+	}		
+
+	Acad::ErrorStatus GetAllLines() {
+
+		ads_name ssname;
+		struct resbuf rb;
+
+		rb.restype = 0;
+		rb.resval.rstring = L"LINE";
+		rb.rbnext = NULL;
+
+		acedSSGet(L"X", NULL, NULL, &rb, ssname);
+
+		int numObj;
+		acedSSLength(ssname, &numObj);
+
+		AcDbObjectIdArray lineIds_(numObj);
+		Acad::ErrorStatus es;
+
+		for (int i = 0; i < numObj; i++) {
+			ads_name objName;
+			acedSSName(ssname, i, objName);
+
+			es = acdbGetObjectId(lineIds_[i], objName);
+
+			if (es == Acad::eOk) {
+				continue;
+			}
+
+
+			if (es != Acad::eOk) {
+				ads_name objName;
+				acedSSName(ssname, i, objName);
+
+				ads_point pt;
+				acedGetPoint(NULL, L"\nВыберите точку:", pt);
+			}
+		}
+		acedSSFree(ssname);		
+
+		int b = lineIds_.physicalLength();
+		
+		for (int i = 0; i < lineIds_.physicalLength(); i++) {
+			lineIds.append(lineIds_[i]);
+		}		
+		return Acad::eOk;
+	}
+
+	void searchForOverlays() {		
+
+		for (int i = 0; i < lineIds.logicalLength(); i++) {
+			for (int j = i+1; j < lineIds.logicalLength(); j++) {
+				AcDbLine* pLine1 = nullptr;
+				AcDbLine* pLine2 = nullptr;
+				Acad::ErrorStatus es1 = acdbOpenObject(pLine1, lineIds[i], AcDb::kForRead);
+				Acad::ErrorStatus es2 = acdbOpenObject(pLine2, lineIds[j], AcDb::kForRead);
+				if (es1 == Acad::eOk && es2 == Acad::eOk) {
+
+					AcGeVector3d dir1 = pLine1->endPoint() - pLine1->startPoint();
+					AcGeVector3d dir2 = pLine2->endPoint() - pLine2->startPoint();
+
+					dir1.normalize();
+					dir2.normalize();
+
+					
+
+					if (fabs(dir1.dotProduct(dir2)) >= 0.9999) {
+
+						collission = true;
+
+						const ACHAR* myString = _T("There is a collision!");
+						AcCmColor* MyColor = new AcCmColor;
+						MyColor->setRGB(255, 0, 0);
+
+						AcDbText* pText = new AcDbText();
+						pText->setPosition(pLine1->startPoint());
+						pText->setTextString(myString);
+						pText->setHeight(3.5);
+						pText->setColor(*MyColor);	
+
+						AcDbBlockTable* pBlockTable;
+						if (acdbHostApplicationServices()->workingDatabase()->getSymbolTable(pBlockTable, AcDb::kForRead) == Acad::eOk) {
+							AcDbBlockTableRecord* pBlockTableRecord;
+							if (pBlockTable->getAt(ACDB_MODEL_SPACE, pBlockTableRecord, AcDb::kForWrite) == Acad::eOk) {
+								AcDbObjectId textId;
+								pBlockTableRecord->appendAcDbEntity(textId, pText);
+								pBlockTableRecord->close();
+							}
+							pBlockTable->close();
+						}
+
+						pText->close();						
+					}
+
+					pLine1->close();
+					pLine2->close();
+				}
+			}
+		}
+	
+
+		
+	}
+
+	void showMyWindow() {		
+			CAdUiDialog* MyWindow = new CAdUiDialog(102, nullptr, nullptr);
+			MyWindow->Create(102);
+			MyWindow->ShowWindow(SW_SHOW);
+			collission = false;
+		
+	}
+
+	static void ADSKMyPlagin_StartTest() {
+		if (g_pApp) {
+			g_pApp->showMyWindow();			
+		}
+	}
+
+
+private:
+	bool collission = false;
+	std::chrono::steady_clock::time_point m_lastRunTime;
+	AcDbObjectIdArray lineIds;	
+};
+
+IMPLEMENT_ARX_ENTRYPOINT(CArxProject3App)
+
+ACED_ARXCOMMAND_ENTRY_AUTO(CArxProject3App, ADSKMyPlagin, _StartTest, _StartTest, ACRX_CMD_TRANSPARENT, NULL)
+
+
---
+++ ArxProject3/resource.h
@@ -0,0 +1,18 @@
+//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется ArxProject3.rc
+//
+#define IDS_PROJNAME                    100
+#define IDD_DIALOG1                     102
+#define IDC_TEXTBOX1                    102
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        108
+#define _APS_NEXT_COMMAND_VALUE         32768
+#define _APS_NEXT_CONTROL_VALUE         103
+#define _APS_NEXT_SYMED_VALUE           102
+#endif
+#endif
